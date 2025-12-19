190ef127cabaaf4b0bfba70b670efeda2a5309a0
buivominhtruong <67497520+buivominhtruong@users.noreply.github.com>
2025-12-19 16:02:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ const observer = new IntersectionObserver((entries) => {
 }, {
     threshold: 0.1 // Kích hoạt khi 10% phần tử xuất hiện
 });
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-list');
 
+// Mở/Đóng menu khi click vào nút Hamburger
+menu.addEventListener('click', function() {
+    menuLinks.classList.toggle('active');
+    
+    // Hiệu ứng xoay nút Hamburger (tùy chọn)
+    this.classList.toggle('is-active');
+});
+
+// Tự động đóng menu khi người dùng chọn 1 mục
+document.querySelectorAll('.nav-list a').forEach(n => n.addEventListener('click', () => {
+    menuLinks.classList.remove('active');
+}));
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
